5f38a2b93f79b653d3ab70178906f0460bcec79e
Paulo Ricardo Alves <pauloricardooalves@gmail.com>
2024-04-18 11:34:33 -0300

    Resolução da lista de exercicios de algoritmos e programação

---
+++ Exercicios_propostos_1.py
@@ -0,0 +1,32 @@
+##   QUESTÃO 1   ## --------------------
+
+# idade = 18
+
+# if idade >= 18:
+#     print("Você é maior de idade")
+# else:
+#     print("Você é menor de idade")
+
+##   QUESTÃO 2   ## --------------------
+
+# salario = int(input("Qual o seu salário?"))
+# bonus = 0
+
+# if salario < 2000:
+#   bonus = 0.1 * salario
+
+# print("o bonus é de: ", + bonus)
+
+##   QUESTÃO 3   ###  --------------------
+
+n1 = float(input("Insira a primeira nota"))
+n2 = float(input("Insira a segunda nota"))
+n3 = float(input("Insira a terceira nota"))
+media = 0
+
+media = (n1 + n2 + n3) /3
+
+if media >= 7:
+  print("Aprovado, sua nota é: ", media)
+else:
+  print("Reprovado, sua nota e: ", media)
